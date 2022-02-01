9ec80cff632e9ed3e3e56bf96b43b7099774c58e
Manuel Giffels <giffels@gmail.com>
2022-02-01 16:38:56 +0100

    Better workaround for --nv in nested containers

--- wlcg-wn/singularity_nv_setup.sh
+++ wlcg-wn/singularity_nv_setup.sh
@@ -3,14 +3,5 @@
 # this workaround ensures that --nv even works in nested singularity containers
 # details can be found in https://github.com/apptainer/singularity/issues/5759 
 
-TMPD=$(mktemp -d)
-
-cat << EOF > ${TMPD}/ldconfig
-#!/bin/bash
-exec /usr/sbin/ldconfig -C ${TMPD}/ld.so.cache \$@
-EOF
-
-chmod +x ${TMPD}/ldconfig
-
-PATH=${TMPD}:${PATH}
-ldconfig ${LD_LIBRARY_PATH}
+[[ :$LD_LIBRARY_PATH: == *":/.singularity.d/libs:"* ]] || LD_LIBRARY_PATH=/.singularity.d/libs:${LD_LIBRARY_PATH}
+/usr/local/bin/ldconfig /.singularity.d/libs
